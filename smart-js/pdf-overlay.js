92488b352daed39c80d77b0b7eb4be2a05690b7d
adeiji <adeiji@yahoo.com>
2014-07-09 12:48:27 -0700

    Made it so that the fast rates box is added after the html is generated

--- smart-js/pdf-overlay.js
+++ smart-js/pdf-overlay.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
 	$('#pdf-overlay').hide();
 	
+	$('#pdf-overlay').append('<div id = "main-close-window">Close Window</div>' +
+    		'<div id = "prices-pdf">' +    			
+    			'<embed src="pdf/prices.pdf" id = "pdf">' +
+    		'</div>"');
+
 	$('#pdf-overlay').click(function () {
         $('#pdf-overlay').hide();
     });
